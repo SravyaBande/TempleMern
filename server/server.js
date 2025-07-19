21e712046c4428ae7e3e85193b27458ba36eac86
SravyaBande <sravyabande@gmail.com>
2025-07-19 13:55:47 +0530

    updated package.json

--- server/server.js
+++ server/server.js
@@ -1,26 +1,38 @@
-
-
-const express =require('express');
+const express = require('express');
 const mongoose = require('mongoose');
-const cors=require('cors');
+const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
 const eventRoutes = require('./routes/eventRoutes');
 app.use('/events', eventRoutes);
 
-app.get('/',(req,res)=> {
-    res.send('temple backend is running');
-});
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  const __dirname1 = path.resolve();
+  app.use(express.static(path.join(__dirname1, '/client/build')));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname1, 'client', 'build', 'index.html'));
+  });
+} else {
+  // Development route
+  app.get('/', (req, res) => {
+    res.send('Temple backend is running');
+  });
+}
+
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
-    .then(()=> console.log('MongoDB connected'))
-    .catch((err)=> console.error('MondoDb error',err));
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB error', err));
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
-
+// Server Listener
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
